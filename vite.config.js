e01a68c5aee2e4e764d57c29c8e642af10ba3e32
Suliman Khan <suliman.soft.eng@gmail.com>
2025-08-06 21:42:27 +0500

    change the url again

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/3d-flowers/",
   plugins: [react(), tailwindcss()],
-  base: "/"
 })
